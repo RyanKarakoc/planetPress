f9b405e3dcf5696a1de8024aabd77089caaa82e6
Ryan <kochy@hotmail.co.uk>
2023-11-25 14:40:42 +0000

    feat: created a scraper for natGeoEnviroment

---
+++ utils/natGeoScraper.js
@@ -0,0 +1,64 @@
+import puppeteer from "puppeteer";
+
+const natGeoScraper = async () => {
+  // Launch the browser and open a new blank page
+  const browser = await puppeteer.launch({ headless: "new" });
+  const page = await browser.newPage();
+  // Set screen size
+  await page.setViewport({ width: 1080, height: 1024 });
+  try {
+    // Navigate the page to a URL
+    await page.goto(
+      "https://www.nationalgeographic.com/environment/topic/climate-change"
+    );
+
+    const selector = ".GridPromoTile__Row";
+
+    await page.waitForSelector(selector);
+    const el = await page.$(selector);
+
+    const articleData = await page.evaluate(() => {
+      const extractedData = [];
+      const promoTiles = document.querySelectorAll(
+        ".PromoTile__Title--truncated"
+      );
+
+      promoTiles.forEach((tile) => {
+        const headline = tile.querySelector("span").textContent.trim();
+        const url = tile
+          .closest("article")
+          .querySelector("a")
+          .getAttribute("href");
+        const img_url = tile
+          .closest("article")
+          .querySelector(".BackgroundImage__Image--highRes")
+          .style.backgroundImage.replace(/.*\s?url\([\'\"]?/, "")
+          .replace(/[\'\"]?\).*/, "");
+
+        extractedData.push({ headline, url, img_url });
+      });
+
+      return extractedData;
+    });
+
+    // adding date and previews
+    articleData.map((article) => {
+      if (!article.preview) {
+        article.preview = "No preview available.";
+      }
+      if (!article.date) {
+        article.date = new Date();
+      }
+    });
+
+    console.log(articleData);
+
+    
+  } catch (err) {
+    console.log("scrape failed", err);
+  } finally {
+    await browser.close();
+  }
+};
+
+natGeoScraper();
