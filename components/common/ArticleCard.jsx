296d278117ceca4c0350427af01f6eda4c3b4ef9
LpgLevin <lpglevin@gmail.com>
2023-11-25 22:21:32 +0000

    AllArticles showing on articles screen!

--- components/common/ArticleCard.jsx
+++ components/common/ArticleCard.jsx
@@ -1,25 +1,59 @@
 
 import * as React from 'react';
+import { Linking } from 'react-native';
 import { Avatar, Button, Card, Text } from 'react-native-paper';
-import { articles } from '../../dummy-data/index.js';
+import { StyleSheet } from 'react-native';
 
 const LeftContent = props => <Avatar.Icon {...props} icon="folder" />
 
-export default function ArticleCard() {
+export default function ArticleCard({ id, headline, preview, img_url, url}) {
+
+    const handleOnPress = () => {
+        console.log("clicked");
+        Linking.openURL(url);
+    }
 
     return (
         <Card>   
             <Card.Content>
-                <Text variant="titleLarge">Headline</Text>
-                <Text variant="bodyMedium">First few lines of the article Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur, molestiae vero perspiciatis rem dicta quia dolor...</Text>
+                <Text style={styles.headline} variant="titleLarge">{headline}</Text>
+                <Text style={styles.preview} variant="bodyMedium">{preview}</Text>
             </Card.Content>
-            <Card.Cover source={{ uri: 'https://picsum.photos/700' }} />
-            <Card.Actions>
-                <Button>Cancel</Button>
-                <Button>Ok</Button>
+            <Card.Cover source={{ uri: `${img_url}`}} />
+            <Card.Actions style={styles.bottom}>
+                <Button textColor='white' style={styles.button} onPress={handleOnPress} >Read full article</Button>
+                <Button style={styles.button}
+                    icon="heart"
+                > </Button>
             </Card.Actions>
         </Card>
 
     );
 
 };
+
+const styles = StyleSheet.create({
+    bottom: {
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+      backgroundColor: '#8fce00',
+      position: 'absolute',
+      left: 0,
+      right: 0,
+      bottom: 0,
+    },
+    button: {
+        backgroundColor: '#274e13',
+        border: 'solid',
+        borderColor: '#274e13',
+        borderTopColor: '#274e13',
+    },
+    headline: {
+        color: '#274e13',
+        fontWeight: 'bold',
+    },
+    preview: {
+        color: '#274e13',
+    }
+  });
