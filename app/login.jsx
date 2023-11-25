040d6932ee1815c15932d375be68347f540b6ee5
Ryan <kochy@hotmail.co.uk>
2023-11-25 17:52:39 +0000

    refactor: added missing imports

--- app/login.jsx
+++ app/login.jsx
@@ -1,6 +1,6 @@
 import { Button, TextInput } from "react-native-paper";
 import Header from "../components/common/Header";
-import { View } from "react-native";
+import { StyleSheet, View } from "react-native";
 import { login, signUp } from "../utils/authFunctions";
 import { useEffect, useState } from "react";
 import { router } from "expo-router";
@@ -45,8 +45,8 @@ export default function Login() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
