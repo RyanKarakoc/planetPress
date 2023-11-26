9df30ec3d10157297c54703957d5a8163eb7e90d
LpgLevin <lpglevin@gmail.com>
2023-11-26 17:13:09 +0000

    wip: Lily passing MyStuff page over to Bilaal

--- app/my-articles-and-events.jsx
+++ app/my-articles-and-events.jsx
@@ -1,13 +1,62 @@
-import MyArticles from '../components/my-articles-and-events/MyArticles';
-import MyEvents from '../components/my-articles-and-events/MyEvents';
+import Header from "../components/common/Header";
+import NavBar from "../components/common/NavBar/NavBar";
+import { View } from "react-native";
+import MyArticles from "../components/my-articles-and-events/MyArticles";
+import MyEvents from "../components/my-articles-and-events/MyEvents";
+import * as React from 'react';
+import { SafeAreaView, StyleSheet } from 'react-native';
+import { SegmentedButtons } from 'react-native-paper';
+import { useState } from 'react';
 import ScreenLayout from '../components/common/ScreenLayout';
 
+
 export default function MyArticlesAndEvents() {
-  return (
-    <ScreenLayout>
-      {/* Make tab-switching functionality */}
-      <MyArticles />
-      <MyEvents />
-    </ScreenLayout>
-  );
-}
+
+  const Articles = <MyArticles/>;
+  const Events = <MyEvents/>;
+
+  const [articlesOrEvents, setArticlesOrEvents] = React.useState(null);
+
+    return (
+      
+      <ScreenLayout>
+          <SafeAreaView style={styles.container}>
+            <SegmentedButtons
+              style={styles.buttons}
+              value={articlesOrEvents}
+              onValueChange={setArticlesOrEvents}
+              buttons={[
+                {
+                  value: Articles,
+                  label: 'My Articles',
+                  icon: 'newspaper',
+                },
+                {
+                  value: Events,
+                  label: 'My Events',
+                  icon: 'calendar',
+                },
+              ]}
+            />
+      </SafeAreaView>
+      
+        {articlesOrEvents}
+       
+     
+      </ScreenLayout>
+      
+    
+    );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: "100%",  
+  },
+  buttons: {
+    
+    
+  },  
+
+});
